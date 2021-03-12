c9bc95c9036451fbfd298f097e2ae3c87ec8964a
AJ <joachim.liedtke@t-online.de>
2021-03-12 17:47:37 +0100

    style(containers/events/events.js): clean up code
    reformat html for better readability

--- client/src/containers/Events/Events.js
+++ client/src/containers/Events/Events.js
@@ -51,12 +51,12 @@ export default function Events({ value }) {
   return (
     <Box bg={'custom.100'} h={'100vh'}
          bgImage="url('https://res.cloudinary.com/dujun1hoe/image/upload/v1615228154/event-s/gradient-background-26046-26731-hd-wallpapers.jpg_cenrqe.png')"  
-         bgSize="cover"
-         backgroundRepeat="no-repeat"
+         bgSize="cover" backgroundRepeat="no-repeat"
      >
       <Flex h={'50px'} align={'flex-end'} justify={'center'} >
         <motion.div initial="hidden" animate="visible"
-                    variants={{ hidden: { scale: .8, opacity: 0 }, visible: { scale: 1, opacity: 1, transition: { delay: .4 }} }} >
+                    variants={{ hidden: { scale: .8, opacity: 0 }, visible: { scale: 1, opacity: 1, transition: { delay: .4 }} }}
+        >
           <Heading  color={'white'} align={'center'} justify={'center'} >Find an event</Heading>
         </motion.div>
       </Flex>
@@ -78,7 +78,8 @@ export default function Events({ value }) {
             </Flex>
 
             <Flex borderRadius="md" flex={2} p={8} flexDirection={'column'}
-                  justifyContent={'end'} bg={'transparent'} h={'80vh'} >
+                  justifyContent={'end'} bg={'transparent'} h={'80vh'}
+            >
               <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
 
               <Box w={'100%'} overflow={'scroll'} h={'100vh'} mt={10} >
