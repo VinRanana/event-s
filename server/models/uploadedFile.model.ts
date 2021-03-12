62ad2cd5f9e7a5cfc07791d34143b953594447c5
VinRanana <davinderrana@hotmail.co.uk>
2021-03-12 19:22:01 +0000

    refactor(models/uploadedfile.model.ts): converted to typescript

--- server/models/uploadedFile.model.ts
+++ server/models/uploadedFile.model.ts
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const fileSchema = new Schema(
@@ -12,4 +12,4 @@ const fileSchema = new Schema(
 );
 
 const UploadedFile = mongoose.model('UploadedFile', fileSchema);
-module.exports = UploadedFile;
+export default UploadedFile;
