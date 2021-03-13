e91470df58b07ea0e734583bb90221cc754f0bcb
VinRanana <davinderrana@hotmail.co.uk>
2021-03-13 12:07:18 +0000

    refactor(models/index.ts): convert to typescript
    change file type, and add type to error

---
+++ server/models/index.ts
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const { dbName, host, dbPort } = require('../config');
+
+module.exports = mongoose.connect(
+  `mongodb://${host}:${dbPort}/${dbName}`, 
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  },
+  (err: PromiseRejectionEvent) => {
+    if (err) {
+      console.log(`😞 Sorry, something went wrong! ${err}`); // eslint-disable-line no-console
+    } else {
+      console.log(`🦆 Database (sessions) connected @ port ${dbPort}!`); // eslint-disable-line no-console
+    }
+  }
+);
