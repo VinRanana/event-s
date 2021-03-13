220a2eeccdd26cbb4362888a0cbd8d22104e1eb1
AJ <joachim.liedtke@t-online.de>
2021-03-13 16:09:08 +0100

    chore(eventcard.tsx): refactor to typescript

--- client/src/components/Event/EventCard.tsx
+++ client/src/components/Event/EventCard.tsx
@@ -4,14 +4,23 @@ import { Link as RouterLink } from 'react-router-dom';
 import moment from 'moment';
 import { motion } from 'framer-motion';
 
+interface ValueInterface {
+  value: {
+    _id: string;
+    name: string;
+    photo: string;
+    location: string;
+    date: string;
+  }
+}
 
-function EventCard ({value}) {
+function EventCard ({value}: ValueInterface) {
 
   return (
     <Box maxW={'40%'} h={'250px'} maxH={'50%'} w={'25%'} bg={useColorModeValue('white', 'gray.900')}
          boxShadow={'2xl'} m={2} borderRadius="md" p={3} overflow={'hidden'}>
 
-      <motion.div animate={{ scale: [1, 0.8, 1], duration: 0.5 }} 
+      <motion.div animate={{ scale: [1, 0.8, 1]/*, duration: 0.5*/ }} 
                   transition={{ duration: 0.5, ease: "easeInOut", times: [0, 0.2, 0.5, 0.8, 0] }}
                   whileHover={{ scale: 1.1, transition: { duration: .2 } }}>
 
