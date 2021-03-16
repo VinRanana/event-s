2c5958ddb1141c651243bd72c7dba23bc3f5deac
AJ <joachim.liedtke@t-online.de>
2021-03-16 13:29:50 +0100

    test(navbar): modify navbar test

--- client/src/components/Navbar.test.js
+++ client/src/components/Navbar.test.js
@@ -6,8 +6,8 @@ import NavigationBar from "./Navbar";
 test('renders Navbar', () => {
   const { container, getByText, getByTestId } = render(<Router><NavigationBar /></Router>);
 
-  const homeButton = getByText('Home');
-  const eventsButton = getByText('Events');
+  const homeButton = getByText(/home/i);
+  const eventsButton = getByText(/events/i);
   const avatarButton = getByTestId('avatar');
 
   expect(homeButton).toBeInTheDocument();
